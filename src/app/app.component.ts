1451d9a594241485740d901edb4a883c0a48852f
Steve Cohen <cohensteve@hotmail.fr>
2021-09-16 11:16:08 +0200

    add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import { faFacebook, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faMailBulk } from "@fortawesome/free-solid-svg-icons";
+import {version, repository, name} from '../../package.json';
 import {Board} from "@fuwu-yuan/bgew";
 import {BouncingBallStep} from "./bouncing-ball/bouncing-ball.step";
 
@@ -8,12 +11,19 @@ import {BouncingBallStep} from "./bouncing-ball/bouncing-ball.step";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'particles';
+  title = 'Particles';
+  currentYear: number = new Date().getFullYear();
+  faFacebook = faFacebook;
+  faTwitter = faTwitter;
+  faLinkedin = faLinkedin;
+  faMailBulk = faMailBulk;
+  name = name;
+  sources = repository.url;
 
   ngOnInit(): void {
     let board = new Board("Particles", "1.0.0", 900, 900, document.getElementById("game-container"), "#FFF");
     board.config.game.FPS = 60;
-    board.debug.stats = true;
+    board.debug.stats = false;
     board.debug.collision = false;
     let bouncingBallStep = new BouncingBallStep(board);
 
@@ -21,6 +31,10 @@ export class AppComponent implements OnInit {
 
     board.step = bouncingBallStep;
     board.start();
+  }
+
+  getNameAndVersion() {
+    return `${this.title} v${version}`;
   }
 
 }
